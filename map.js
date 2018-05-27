63e41d260cf5bcf4d8a3e3e5a1e037363fc49091
Omar Cesar <omar-nav@users.noreply.github.com>
2018-05-27 16:59:27 -0500

    Initial commit

---
+++ map.js
@@ -0,0 +1,41 @@
+var mymap = L.map('mapid').setView([13.804318, -86.272771], 2.9);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 7,
+    opacity: .7,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1Ijoib21hci1uYXZhcnJvIiwiYSI6ImNpanN2ZWZxZzBoa291eWx4ZWdsajl1OGIifQ.SH4OG9811nirTGJ3rE4DHw'
+}).addTo(mymap);
+function choroplethize(d) {
+    return d > 43.6 ? '#bd0026' :
+        d > 26.0 ? '#f03b20' :
+            d > 15 ? '#fd8d3c' :
+                d > 7.4 ? '#fecc5c' :
+                    d > 0 ? '#ffffb2' :
+                        <!-- if values are null keep transparent-->
+                        '#1C00ff00';
+}
+function styleHomicides(feature) {
+    return {
+        weight: .75,
+        opacity: 0.5,
+        color: 'grey',
+        dashArray: '0',
+        fillOpacity: 0.9,
+        fillColor: choroplethize(feature.properties.refactor_4)
+    }
+}
+function geojsonPopup2000(feature, layer){
+    if(feature.properties.SOVEREIGNT){
+        layer.bindPopup('Country:   ' + feature.properties.SOVEREIGNT + '<br>2000 Total Homicides:   '+ feature.properties.refactor_3 + '<br>2000 Homicide Rate:   '+ feature.properties.refactor_4 + '%')
+    }
+}
+
+L.geoJSON([homicides2000], {
+    style: styleHomicides,
+    onEachFeature: geojsonPopup2000,
+    pointToLayer: function (feature, latlng) {
+        return L.marker(latlng);
+    }
+}).addTo(mymap);
